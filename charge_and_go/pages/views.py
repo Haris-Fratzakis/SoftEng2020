97c61a369b447cb7d73518aac088a095bd224d88
p3ro <perogior@gmail.com>
2021-01-11 00:30:21 +0200

    Add base.html and login.html

--- charge_and_go/pages/views.py
+++ charge_and_go/pages/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
+
+
 # Create your views here.
 
 def monthly_bill_view(request):
     context = {
-    "curr_year": datetime.datetime.now().year,
+        "curr_year": datetime.datetime.now().year,
     }
     return render(request, "monthly_bill.html", context)
+
+
+def login_view(request):
+    return render(request, "login.html")
