59c8f74e3c6efe788884ce376685a0009973b699
CharalamposF <75033654+CharalamposF@users.noreply.github.com>
2021-01-11 21:12:11 +0200

    establishes mysql database with django

--- charge_and_go/back_end/admin.py
+++ charge_and_go/back_end/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import TestObject
+#from .models import TestObject
 
 # Register your models here.
 
-admin.site.register(TestObject)
+#admin.site.register(TestObject)
