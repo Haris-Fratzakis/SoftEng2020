82007ee184215c860a9d569b1c8b2b7eeb206846
CharalamposF <75033654+CharalamposF@users.noreply.github.com>
2021-01-11 22:35:33 +0200

    establishes mysql database with django

--- charge_and_go/back_end/admin.py
+++ charge_and_go/back_end/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-#from .models import TestObject
+from .models import userAccount
 
 # Register your models here.
 
-#admin.site.register(TestObject)
+admin.site.register(userAccount)
--- charge_and_go/back_end/models.py
+++ charge_and_go/back_end/models.py
@@ -5,4 +5,13 @@ from django.db import models
 class userAccount(models.Model):
     first_name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
-    birth_date = models.IntegerField() #change later
+    birth_date = models.DateField()
+    age = models.IntegerField()
+    GENDER_CHOICES = (
+        ("FEMALE", "Female"),
+        ("MALE", "Male"),
+        ("OTHER", "Other"),
+    )
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    city = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
